cb776d1b3ec61a119e5d1512fe34a6f1a63b49d5
unknown <chaukiyal98@gmail.com>
2019-01-30 11:25:35 +0530

    Init commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    
+    mongoURI:'mongodb://Naman:naman1@ds253804.mlab.com:53804/storybooks-dev',
+    googleClientID:'621271710902-8ia26p9b89pgn6i28cr158q15ea1cki0.apps.googleusercontent.com',
+   googleClientSecret:'2u7ed_CWNQRtfiLfEUGhOj5E'
+}
